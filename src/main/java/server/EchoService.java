bde54460a45107a7b0a75cd1197c3867672a5eed
nisioka <nishioka.daisuke@gmail.com>
2019-06-03 07:49:11 +0900

    コメント追記。

--- src/main/java/server/EchoService.java
+++ src/main/java/server/EchoService.java
@@ -8,6 +8,7 @@ public class EchoService extends EchoServiceGrpc.EchoServiceImplBase {
 
     @Override
     public void echo(Echo.EchoRequest request, StreamObserver<Echo.EchoResponse> responseObserver) {
+        // リクエストからmessageを受け取り、そのままレスポンスのmessageとしてセットする。
         Echo.EchoResponse echo = Echo.EchoResponse.newBuilder().setMessage(request.getMessage()).build();
         responseObserver.onNext(echo);
         responseObserver.onCompleted();

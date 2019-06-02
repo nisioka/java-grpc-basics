5ec7bcb5e16aa7890e90f6d4c3c8443118deb569
nisioka <nishioka.daisuke@gmail.com>
2019-06-02 20:27:54 +0900

    gRPC server実装。 https://qiita.com/disc99/items/3fdfe5c1c1170871221a

---
+++ src/main/java/server/EchoService.java
@@ -0,0 +1,15 @@
+package server;
+
+import echo.Echo;
+import echo.EchoServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class EchoService extends EchoServiceGrpc.EchoServiceImplBase {
+
+    @Override
+    public void echo(Echo.EchoRequest request, StreamObserver<Echo.EchoResponse> responseObserver) {
+        Echo.EchoResponse echo = Echo.EchoResponse.newBuilder().setMessage(request.getMessage()).build();
+        responseObserver.onNext(echo);
+        responseObserver.onCompleted();
+    }
+}

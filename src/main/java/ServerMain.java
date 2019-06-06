6353952b2da95ec71fe733568306bc5aac82205d
nishi <daisuke@alpaca.ai>
2019-06-06 09:01:48 +0900

    java11対応。

--- src/main/java/ServerMain.java
+++ src/main/java/ServerMain.java
@@ -1,6 +1,5 @@
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import io.grpc.netty.NettyServerBuilder;
 import server.EchoService;
 
 import java.io.IOException;
@@ -12,12 +11,12 @@ public class ServerMain {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         final int portNumber = 50051;
-        NettyServerBuilder builder = (NettyServerBuilder)ServerBuilder.forPort(portNumber)
-                .addService(new EchoService()); // gRPCのサービスを紐づける。
-        Server server = builder.build();
+        Server server = ServerBuilder.forPort(portNumber)
+                .addService(new EchoService()) // gRPCのサービスを紐づける。
+                .build();
 
         server.start();
-        System.out.printfln("start server on port:%s", portNumber);
+        System.out.println("start server on port:" + portNumber);
         server.awaitTermination();
     }
 }

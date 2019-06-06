5a8ac92ffea7c5079f8256bac9c87ebda3985df4
nishi <daisuke@alpaca.ai>
2019-06-06 13:40:40 +0900

    コメント追記。リファクタリング。

--- src/main/java/ClientMain.java
+++ src/main/java/ClientMain.java
@@ -3,14 +3,19 @@ import echo.EchoServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+/**
+ * クライアントからサーバへリクエストを送る。
+ */
 public class ClientMain {
     public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
-                .usePlaintext(true)
+        final int portNumber = 50051;
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", portNumber)
+                .usePlaintext(true) // 簡単のためサーバ証明書なしのローカル通信とする。
                 .build();
 
         EchoServiceGrpc.EchoServiceBlockingStub stub = EchoServiceGrpc.newBlockingStub(channel);
 
+        // "hello world."メッセージをリクエストする。
         Echo.EchoRequest request = Echo.EchoRequest.newBuilder().setMessage("hello world.").build();
 
         Echo.EchoResponse response = stub.echo(request);
